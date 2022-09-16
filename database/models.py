0188d96f5a206cda02d0195313c87389ad2f7c84
Andrey Veselov <vesell4123@mail.ru>
2022-09-17 00:50:00 +0300

    build model in db

---
+++ database/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, Text, Boolean
+
+from base import Base, Engine
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    post_id = Column('post_id', Integer, primary_key=True)
+    owner_id = Column('owner_id', Integer)
+    group_name = Column('group_name', Text)
+    quantity_comments = Column('quantity_comments', Integer)
+    photo = Column('photo', Boolean)
+    quantity_like = Column('quantity_like', Integer)
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=Engine)
