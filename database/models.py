5bec77e45e6a0e7298be4e943dfccc3dab27ede9
Andrey Veselov <vesell4123@mail.ru>
2022-09-18 04:18:05 +0300

    create models: Post, Comment

--- database/models.py
+++ database/models.py
@@ -1,18 +1,28 @@
-from sqlalchemy import Column, Integer, Text, Boolean
+from sqlalchemy import Column, Integer, BigInteger, Text, Boolean
 
-from loader import Base, Engine
+from loader import Base, engine
 
 
-class Users(Base):
+class User(Base):
     __tablename__ = 'users'
 
     post_id = Column('post_id', Integer, primary_key=True)
     owner_id = Column('owner_id', Integer)
-    group_name = Column('group_name', Text)
+    group = Column('group', Text)
     quantity_comments = Column('quantity_comments', Integer)
+    likes = Column('likes', Integer)
+    views = Column('views', BigInteger)
     photo = Column('photo', Boolean)
-    quantity_like = Column('quantity_like', Integer)
+    post_text = Column('text', Text)
 
 
-if __name__ == "__main__":
-    Base.metadata.create_all(bind=Engine)
+class Comment(Base):
+    __tablename__ = 'comments'
+
+    comment_id = Column('comment_id', Integer, primary_key=True)
+    post_id = Column('post_id', Integer)
+    comment_text = Column('text', Integer)
+
+
+def create_db():
+    Base.metadata.create_all(bind=engine)
