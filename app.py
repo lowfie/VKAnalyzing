37330768ecbe48e1d525321af45715d089254226
Andrey Veselov <vesell4123@mail.ru>
2022-09-18 04:19:31 +0300

    main file

---
+++ app.py
@@ -0,0 +1,10 @@
+from aiogram import executor
+from loader import dp
+
+
+async def on_startup(dispatcher):
+    print('Бот запущен')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
