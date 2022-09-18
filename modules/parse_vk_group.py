03a8eb2ec47d3e65af8a2f37feb94b414d63e220
Andrey Veselov <vesell4123@mail.ru>
2022-09-19 02:51:52 +0300

    entering data into a table

--- modules/parse_vk_group.py
+++ modules/parse_vk_group.py
@@ -1,7 +1,9 @@
+import time
+
 from data.config import VK_TOKEN
 import requests
 
-from database.models import Post, create_db
+from database.models import Post, Comment, create_db
 from loader import session
 
 group_name = input('Введите название группы: ')
@@ -21,30 +23,41 @@ class VkParser:
         for item in items:
             self.posts.append({'post_id': item['id'], 'owner_id': item['owner_id']})
 
-            post = Post(
-                post_id=item['id'],
-                owner_id=item['owner_id'],
-                group=group_name,
-                quantity_comments=item['comments']['count'],
-                likes=item['likes']['count'],
-                views=item['views']['count'],
-                photo=True,
-                post_text=item['text']
-            )
-            '''Когда ставлю autocommit=True, данные всё равно не сохраняются в бд'''
-            session.add(post)
-            session.commit()
-
+            # post = Post(
+            #     post_id=item['id'],
+            #     owner_id=item['owner_id'],
+            #     group=group_name,
+            #     quantity_comments=item['comments']['count'],
+            #     likes=item['likes']['count'],
+            #     views=item['views']['count'],
+            #     photo=True,
+            #     post_text=item['text']
+            # )
+            # '''Когда ставлю autocommit=True, данные всё равно не сохраняются в бд'''
+            # session.add(post)
+            # session.commit()
 
     def get_wall_comments(self):
-        for post in self.posts:
+        for num, post in enumerate(self.posts, start=1):
             link = self.url + f'wall.getComments?owner_id={post["owner_id"]}&post_id={post["post_id"]}&count=100&sort=desc&access_token={VK_TOKEN}&{self.vk_version}'
-            response = requests.get(link)
-            print(response.text)
+            response = requests.get(link).json()
+            if num % 5 == 0:
+                time.sleep(2)
+
+            for item in response['response']['items']:
+                print(item['text'])
+
+                # comment = Comment(
+                #     comment_id=item['id'],
+                #     post_id=post['post_id'],
+                #     text=item['text']
+                # )
+                # session.add(comment)
+                # session.commit()
 
     def main(self):
         self.get_posts()
-        # self.get_wall_comments()
+        self.get_wall_comments()
 
 
 if __name__ == '__main__':
