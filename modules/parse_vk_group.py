a74c2b430ed74c4a1eb00f1444508ba2f4eb6c1b
Andrey Veselov <vesell4123@mail.ru>
2022-09-19 22:08:34 +0300

    description and modification of the structure

--- modules/parse_vk_group.py
+++ modules/parse_vk_group.py
@@ -4,7 +4,6 @@ from data.config import VK_TOKEN
 import requests
 
 from database.models import Post, Comment, create_db
-from loader import session
 
 group_name = input('Введите название группы: ')
 
@@ -13,47 +12,57 @@ class VkParser:
     def __init__(self):
         self.url = 'https://api.vk.com/method/'
         self.vk_version = 'v=5.131'
-        self.posts = []
+        self.posts_metadata = []
 
     def get_posts(self):
+        """
+        Парсинг данных 40 последних постов из группы вк
+        И занесение данных в бд
+        """
         link = self.url + f'wall.get?domain={group_name}&count=40&access_token={VK_TOKEN}&{self.vk_version}'
         response = requests.get(link).json()
+        # Список из 40 последних постов
         items = [item for item in response['response']['items']]
 
         for item in items:
-            self.posts.append({'post_id': item['id'], 'owner_id': item['owner_id']})
-
-            # post = Post(
-            #     post_id=item['id'],
-            #     owner_id=item['owner_id'],
-            #     group=group_name,
-            #     quantity_comments=item['comments']['count'],
-            #     likes=item['likes']['count'],
-            #     views=item['views']['count'],
-            #     photo=True,
-            #     post_text=item['text']
-            # )
-            # '''Когда ставлю autocommit=True, данные всё равно не сохраняются в бд'''
-            # session.add(post)
-            # session.commit()
+            self.posts_metadata.append({'post_id': item['id'], 'owner_id': item['owner_id']})
+            # Добавление данных в бд
+            post_data = {
+                'id': item['id'],
+                'owner_id': item['owner_id'],
+                'group': group_name,
+                'quantity_comments': item['comments']['count'],
+                'likes': item['likes']['count'],
+                'views': item['views']['count'],
+                'photo': True if 'attachments' in item else False,
+                'text': item['text']
+            }
+            Post.add_post(post_data)
 
     def get_wall_comments(self):
-        for num, post in enumerate(self.posts, start=1):
-            link = self.url + f'wall.getComments?owner_id={post["owner_id"]}&post_id={post["post_id"]}&count=100&sort=desc&access_token={VK_TOKEN}&{self.vk_version}'
+        """
+        Парсинг комментариев поста
+        И занесение данных в бд
+        """
+        for num, post in enumerate(self.posts_metadata, start=1):
+            link = self.url + f'wall.getComments?owner_id={post["owner_id"]}&post_id={post["post_id"]}' \
+                              f'&count=100&sort=desc&access_token={VK_TOKEN}&{self.vk_version}'
             response = requests.get(link).json()
-            if num % 5 == 0:
-                time.sleep(2)
+
+            print(response)
+            # Обход ограничение на 5 запросов в секунду
+            # if num % 5 == 0:
+            #     time.sleep(2)
 
             for item in response['response']['items']:
-                print(item['text'])
-
-                # comment = Comment(
-                #     comment_id=item['id'],
-                #     post_id=post['post_id'],
-                #     text=item['text']
-                # )
-                # session.add(comment)
-                # session.commit()
+                # Добавление данных в бд
+                comment_data = {
+                    'comment_id': item['id'],
+                    'post_id': post['post_id'],
+                    'text': item['text']
+                }
+                print(comment_data)
+                Comment.add_comment(comment_data)
 
     def main(self):
         self.get_posts()
