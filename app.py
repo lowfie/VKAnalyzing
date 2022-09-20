4344ba56a985b39b5b107cf5aeceb630bf2cdb50
Andrey Veselov <vesell4123@mail.ru>
2022-09-20 03:15:31 +0300

    auto-create models

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from aiogram import executor
 from loader import dp
 import handlers
 
-from database.models import create_db
+from database.models import create_tables
 
 
 async def on_startup(dispatcher):
     print('Бот запущен')
-    create_db()
+    create_tables()
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
