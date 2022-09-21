3d35e7ff9010522ab348456d963cfec708666f6b
Andrey Veselov <vesell4123@mail.ru>
2022-09-21 03:11:07 +0300

    preparation for use

--- modules/parse_vk_group.py
+++ modules/parse_vk_group.py
@@ -1,11 +1,11 @@
+import time
+
 from data.config import VK_TOKEN
 import httpx
 
-from database.models import Post, Comment, create_tables
+from database.models import Post, Comment
 from database import Service
 from loader import session
-
-group_name = input('Введите название группы: ')
 
 
 class VkParser:
@@ -14,12 +14,12 @@ class VkParser:
         self.vk_version = 'v=5.131'
         self.posts_metadata = []
 
-    def get_posts(self):
+    def get_posts(self, group):
         """
         Парсинг данных 40 последних постов из группы вк
         И занесение данных в бд
         """
-        link = self.url + f'wall.get?domain={group_name}&count=40&access_token={VK_TOKEN}&{self.vk_version}'
+        link = self.url + f'wall.get?domain={group}&count=40&access_token={VK_TOKEN}&{self.vk_version}'
         response = httpx.get(link).json()
 
         # Список из 40 последних постов
@@ -32,7 +32,7 @@ class VkParser:
             post_data = {
                 'id': item['id'],
                 'owner_id': item['owner_id'],
-                'group': group_name,
+                'group': group,
                 'quantity_comments': item['comments']['count'],
                 'likes': item['likes']['count'],
                 'views': item['views']['count'],
@@ -54,27 +54,18 @@ class VkParser:
                               f'&count=100&sort=desc&access_token={VK_TOKEN}&{self.vk_version}'
             response = httpx.get(link).json()
             # Обход ограничение на 5 запросов в секунду
-            # if num % 5 == 0:
-            #     time.sleep(2)
+            if num % 4 == 0:
+                time.sleep(2)
 
             for item in response['response']['items']:
-                # Добавление данных в бд
-                comment_data = {
-                    'comment_id': item['id'],
-                    'post_id': post['post_id'],
-                    'text': item['text']
-                }
-                if session.query(Comment).filter(Comment.comment_id == comment_data['comment_id']).first() is None:
-                    Service.add_comment(comment_data)
-                else:
-                    Service.update_comment(comment_data)
-
-    def main(self):
-        self.get_posts()
-        self.get_wall_comments()
-
-
-if __name__ == '__main__':
-    create_tables()
-    vk_parser = VkParser()
-    vk_parser.main()
+                if len(item['text'].split()) > 1:
+                    # Добавление данных в бд
+                    comment_data = {
+                        'comment_id': item['id'],
+                        'post_id': post['post_id'],
+                        'text': item['text']
+                    }
+                    if session.query(Comment).filter(Comment.comment_id == comment_data['comment_id']).first() is None:
+                        Service.add_comment(comment_data)
+                    else:
+                        Service.update_comment(comment_data)
