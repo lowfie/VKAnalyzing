5a4775d3f821a7cf8b7a28044bf3df5bcb0a85fb
Andrey Veselov <vesell4123@mail.ru>
2022-09-21 03:11:40 +0300

    init vk parser in telegram command

---
+++ handlers/parsing_group.py
@@ -0,0 +1,22 @@
+from aiogram import types
+from loader import dp
+
+from modules.parse_vk_group import VkParser
+
+
+@dp.message_handler(commands='group')
+async def parser_metadata_group(message: types.Message):
+    text = message.text
+    parser_vk = VkParser()
+
+    if len(text.split()) == 2:
+        group = text.split()[1]
+        parser_vk.get_posts(group)
+        parser_vk.get_wall_comments()
+    else:
+        group = 'Попробуй ещё раз.'
+
+    await dp.bot.send_message(
+        chat_id=message.chat.id,
+        text='Парсинг группы {0} закончился.'.format(group)
+    )
