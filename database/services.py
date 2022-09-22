f7401f1bf3e873590d7117e62b98a6beab832f45
Andrey Veselov <vesell4123@mail.ru>
2022-09-22 04:19:49 +0300

    fix model parameters and adding select func

--- database/services.py
+++ database/services.py
@@ -1,5 +1,5 @@
-from database.models import Post, Comment
 from loader import session
+from sqlalchemy import func
 
 
 class PostService:
@@ -12,7 +12,7 @@ class PostService:
         Функция принимает словарь с данными поста
         и добавляет эти данные бд, если их не существует
         """
-        new_post = Post(
+        new_post = self.post(
             post_id=input_data['id'],
             owner_id=input_data['owner_id'],
             group=input_data['group'],
@@ -35,7 +35,7 @@ class PostService:
         Функция принимает словарь с данными
         и обновляет их
         """
-        post = session.query(Post).filter(Post.post_id == input_data['id']).first()
+        post = session.query(self.post).filter(self.post.post_id == input_data['id']).first()
         if not post:
             raise 'Такого поста нет в бд'
         post.quantity_comments = input_data['quantity_comments']
@@ -46,6 +46,47 @@ class PostService:
         except Exception as err:
             print('Произошла ошибка при обновлении Поста, Текст ошибки:', err)
             session.rollback()
+
+    def select(self, input_data: dict):
+        """
+        Функция принимает словарь со значениями
+        периода времени и группы, этим данным возвращается
+        статистика
+        """
+
+        # Количество постов за период
+        posts = session.query(self.post).filter(
+            self.post.group == input_data['name'],
+            self.post.date >= input_data['date']
+        ).count()
+
+        # Количество постов с фото/видео за период
+        post_with_photo = session.query(self.post).filter(
+            self.post.group == input_data['name'],
+            self.post.date >= input_data['date'],
+            self.post.photo == 'true'
+        ).count()
+
+        # Количество лайков со всех постов за период
+        likes = session.query(func.sum(self.post.likes)).filter(
+            self.post.group == input_data['name'],
+            self.post.date >= input_data['date'],
+        ).scalar()
+
+        # Количество комментариев с постов
+        comments = session.query(func.sum(self.post.quantity_comments)).filter(
+            self.post.group == input_data['name'],
+            self.post.date >= input_data['date'],
+        ).scalar()
+
+        output_data = {
+            'count_post': posts,
+            'posts_with_photo': post_with_photo,
+            'likes': likes,
+            'comments': comments
+        }
+
+        return output_data
 
 
 class CommentService:
@@ -58,12 +99,12 @@ class CommentService:
         Функция принимает словарь метаданных комментария
         И добавляет в бд
         """
-        comment = Comment(
+        new_comment = self.comment(
             comment_id=input_data['comment_id'],
             post_id=input_data['post_id'],
             text=input_data['text']
         )
-        session.add(comment)
+        session.add(new_comment)
         try:
             session.commit()
         except Exception as err:
@@ -75,7 +116,7 @@ class CommentService:
         Функция принимает на вход метаданные комментария
         и обновляет их
         """
-        comment = session.query(Comment).filter(Comment.comment_id == input_data['comment_id']).first()
+        comment = session.query(self.comment).filter(self.comment.comment_id == input_data['comment_id']).first()
         if not comment:
             raise 'Такого комментария нет в бд'
         comment.text = input_data['text']
