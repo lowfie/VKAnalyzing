bf8a20313df6e60aba34098baec72ae36282fd32
Andrey Veselov <vesell4123@mail.ru>
2022-10-10 12:34:52 +0300

    refactor config postgres

--- data/config.py
+++ data/config.py
@@ -3,12 +3,16 @@ import environs
 env = environs.Env()
 env.read_env()
 
+# vk_api token
 VK_TOKEN = env.str('VK_TOKEN')
 
+# telegram_api token
 BOT_TOKEN = env.str('BOT_TOKEN')
 
-USER_DB = env.str('USER_DB')
-PASSWORD_DB = env.str('PASSWORD_DB')
-HOST_PORT = env.str('HOST_PORT')
-DATABASE = env.str('DATABASE')
+# postgres connection
+USER_POSTGRES = env.str('USER_POSTGRES')
+PASSWORD_POSTGRES = env.str('PASSWORD_POSTGRES')
+HOST_POSTGRES = env.str('HOST_POSTGRES')
+PORT_POSTGRES = env.str('PORT_POSTGRES')
+DATABASE_POSTGRES = env.str('DATABASE_POSTGRES')
 
--- loader.py
+++ loader.py
@@ -2,13 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from data.config import USER_DB, PASSWORD_DB, HOST_PORT, DATABASE, BOT_TOKEN
+from data.config import USER_POSTGRES, PASSWORD_POSTGRES, HOST_POSTGRES, PORT_POSTGRES, DATABASE_POSTGRES, BOT_TOKEN
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 # Создание сессии для коммуникации с бд
-engine = create_engine(f"postgresql://{USER_DB}:{PASSWORD_DB}@{HOST_PORT}/{DATABASE}")
+engine = create_engine(
+    f"postgresql://{USER_POSTGRES}:{PASSWORD_POSTGRES}@{HOST_POSTGRES}:{PORT_POSTGRES}/{DATABASE_POSTGRES}")
 session = scoped_session(sessionmaker(bind=engine))
 
 # Создание моделей в бд
