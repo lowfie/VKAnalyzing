254337d08603b649d72e2d2ca4aed43c345ac0e0
Andrey Veselov <vesell4123@mail.ru>
2022-10-10 15:18:19 +0300

    close session redis when shutdown app

--- app.py
+++ app.py
@@ -22,5 +22,11 @@ async def on_startup(dispatcher):
     except Exception as err:
         logger.error(f'Ошибка инициализации БД:\n{err}')
 
+
+async def on_shutdown(dispatcher):
+    await dispatcher.storage.close()
+    await dispatcher.storage.wait_closed()
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
