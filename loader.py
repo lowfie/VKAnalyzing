d40887165b33daeadd158bc825833439cd5ced6f
Andrey Veselov <vesell4123@mail.ru>
2022-10-10 13:39:15 +0300

    change storage connection description: MemoryStorage to RedisStorage2

--- loader.py
+++ loader.py
@@ -2,10 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from data.config import USER_POSTGRES, PASSWORD_POSTGRES, HOST_POSTGRES, PORT_POSTGRES, DATABASE_POSTGRES, BOT_TOKEN
+from data.config import BOT_TOKEN
+from data.config import USER_POSTGRES, PASSWORD_POSTGRES, HOST_POSTGRES, PORT_POSTGRES, DATABASE_POSTGRES
+from data.config import PREFIX_REDIS, PASSWORD_REDIS, HOST_REDIS, PORT_REDIS, DATABASE_REDIS
 
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 # Создание сессии для коммуникации с бд
 engine = create_engine(
@@ -17,9 +19,14 @@ Base = declarative_base()
 Base.query = session.query_property()
 
 # Работа с машиной состояния
-storage = MemoryStorage()
+redis_storage = RedisStorage2(
+    host=HOST_REDIS,
+    port=PORT_REDIS,
+    db=DATABASE_REDIS,
+    pool_size=10,
+    password=PASSWORD_REDIS,
+    prefix=PREFIX_REDIS)
 
 # Подключение к апи телеграмм бота
 bot = Bot(BOT_TOKEN)
-dp = Dispatcher(bot=bot, storage=storage)
-
+dp = Dispatcher(bot=bot, storage=redis_storage)
