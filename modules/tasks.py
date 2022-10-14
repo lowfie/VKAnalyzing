7a4749f3cf7984e955937d97cce68dd9499887f2
Andrey Veselov <vesell4123@mail.ru>
2022-10-14 13:50:18 +0300

    tasks autoparse

---
+++ modules/tasks.py
@@ -0,0 +1,35 @@
+import asyncio
+import aioschedule
+
+from database.models import Group
+from loader import session
+
+from modules.parse_vk_group import VkParser
+
+from loguru import logger
+
+
+async def autoparse_vk():
+    """
+    Собираю данные с таблицы groups, где в колонке autoparse значение True
+    На выходе получается список с группами, который нужно "спарсить"
+    Далее вызываю объект класса VkParser и вызываю функцию run_vk_parser()
+    В неё передаю параметр: группы
+    """
+    vk_parser = VkParser()
+    groups_autoparse = session.query(Group.screen_name).filter(
+        Group.autoparse == 'true'
+    ).all()
+
+    for screen_name in groups_autoparse:
+        logger.info(f'Начался сбор группы {screen_name[0]}')
+        await asyncio.sleep(0.2)
+        await vk_parser.run_vk_parser(screen_name[0])
+
+
+async def schedule():
+    aioschedule.every().day.at('8:00').do(autoparse_vk)
+    aioschedule.every().day.at('20:00').do(autoparse_vk)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
